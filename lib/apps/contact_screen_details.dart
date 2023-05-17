40aa7a8a078109ec72e740a5b0b2c2456f13feb1
Ashraf Ali <ashrafalicitp@gmail.com>
2023-05-17 11:05:25 +0600

    contact list app details screen complter

--- lib/apps/contact_screen_details.dart
+++ lib/apps/contact_screen_details.dart
@@ -1,9 +1,7 @@
-// ignore_for_file: prefer_const_constructors_in_immutables
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
 
 import 'package:contacts_app/apps/modules/contacts.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ContactScreenDetails extends StatelessWidget {
   ContactScreenDetails({super.key, required this.contact});
@@ -15,7 +13,112 @@ class ContactScreenDetails extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(contact.name ?? 'Contact Details'),
+        centerTitle: true,
+        elevation: 0,
       ),
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 180,
+                width: 180,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: NetworkImage(contact.image ?? ''),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.blue),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 60),
+                  child: Column(
+                    children: [
+                      Text(
+                        contact.name ?? "",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        contact.contact ?? '',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white54),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(Icons.phone),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 50,
+                          ),
+                          Container(
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                shape: BoxShape.circle, color: Colors.white54),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Icon(Icons.message),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40),
+                        child: Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.white54,
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 10),
+                              child: Text(
+                                "History",
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
--- lib/apps/home_screen.dart
+++ lib/apps/home_screen.dart
@@ -17,12 +17,41 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final infoData = InfoData();
   List<Contacts> contactsList = [];
+  List<Contacts> filterContactsList = [];
+
+  bool searching = false;
 
   getContacts() {
+    contactsList.clear();
+    filterContactsList.clear();
     for (var item in infoData.contactList) {
       var contact = Contacts.fromJson(item);
 
       contactsList.add(contact);
+      filterContactsList.add(contact);
+    }
+    setState(() {});
+  }
+
+  searchItem(String keyword) {
+    List<Contacts> results = [];
+
+    if (keyword.isEmpty) {
+      results = contactsList;
+    } else {
+      results = contactsList
+          .where((item) =>
+              item.name!.toLowerCase().contains(keyword.trim().toLowerCase()) ||
+              item.contact!
+                  .toLowerCase()
+                  .contains(keyword.trim().toLowerCase()))
+          .toList();
+    }
+    if (filterContactsList.isNotEmpty) {
+      filterContactsList.clear();
+      filterContactsList.addAll(results);
+    } else {
+      filterContactsList = results;
     }
     setState(() {});
   }
@@ -37,31 +66,68 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contacts App '),
+        title: searching
+            ? TextFormField(
+                style: TextStyle(fontSize: 17),
+                decoration: InputDecoration(
+                  hintText: 'Search by name',
+                  fillColor: Colors.white70,
+                  filled: true,
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        searching = false;
+                      });
+                    },
+                    child: Icon(Icons.close),
+                  ),
+                ),
+                onChanged: (value) {
+                  searchItem(value);
+                },
+              )
+            : Text('Contact List'),
         centerTitle: true,
         elevation: 0,
+        actions: [
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                searching = true;
+              });
+            },
+            child: searching ? Container() : Icon(Icons.search),
+          ),
+          SizedBox(
+            width: 10,
+          )
+        ],
       ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-          child: ListView(
-            children: List.generate(contactsList.length, (index) {
-              var contact = contactsList[index];
-              return ContactScreen(
-                  name: contact.name ?? "",
-                  contact: contact.contact ?? "",
-                  image: contact.image ?? "",
-                  onTap: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => ContactScreenDetails(
-                          contact: contact,
-                        ),
-                      ),
-                    );
-                  });
-            }),
-          ),
+          child: filterContactsList.isNotEmpty
+              ? ListView(
+                  children: List.generate(filterContactsList.length, (index) {
+                    var contact = filterContactsList[index];
+                    return ContactScreen(
+                        name: contact.name ?? "",
+                        contact: contact.contact ?? "",
+                        image: contact.image ?? "",
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) => ContactScreenDetails(
+                                contact: contact,
+                              ),
+                            ),
+                          );
+                        });
+                  }),
+                )
+              : Center(
+                  child: Text('No item'),
+                ),
         ),
       ),
     );
